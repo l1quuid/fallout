da5be849e1b812c728de79359812959c3255797c
l1quuid <saa.bizkit@gmail.com>
2024-08-03 14:39:17 +0300

    - create player class

--- objects.py
+++ objects.py
@@ -1,3 +1,6 @@
+import pygame.transform
+import math
+
 from settings import *
 
 class GameSprite(pygame.sprite.Sprite):
@@ -8,11 +11,47 @@ class GameSprite(pygame.sprite.Sprite):
         self.h = h
         self.speed = speed
         self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), (w, h))
+        self.start_image = self.image
 
         self.rect = self.image.get_rect()
         self.rect.center = x, y
 
         self.hitbox = pygame.Rect(self.rect.x, self.rect.y, w / 2, h / 2)
 
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.start_image, angle)
+        self.rect = self.image.get_rect(center=(self.rect.centerx, self.rect.centery))
+
+
     def draw(self):
-        win.blit(self.image)
+        win.blit(self.image, self.rect)
+
+class Player(GameSprite):
+    def __init__(self, image, x, y, w, h, speed):
+        super().__init__(image, x, y, w, h, speed)
+        self.max_hp = 100
+        self.hp = self.max_hp
+
+
+    def update(self):
+        self.hitbox.center = self.rect.center
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] and self.rect.x > 0:
+            self.rect.centerx -= self.speed
+        if keys[pygame.K_d] and self.rect.x < win_width - self.rect.width:
+            self.rect.centerx += self.speed
+        if keys[pygame.K_w] and self.rect.y > 0:
+            self.rect.centery -= self.speed
+        if keys[pygame.K_s] and self.rect.y < win_height - self.rect.height:
+            self.rect.centery += self.speed
+
+        pos = pygame.mouse.get_pos()
+        dx = pos[0] - self.rect.centerx
+        dy = self.rect.centery - pos[1]
+        ang = math.degrees(math.atan2(dy, dx))
+
+        self.rotate(ang - 90)
+
+
