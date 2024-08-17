35db1f448fa587ac6e2819356476ca452a0400aa
l1quuid <saa.bizkit@gmail.com>
2024-08-17 14:25:15 +0300

    - added display of enemies

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from objects import *
-player = Player(player_image, 100, 100, 50, 50, 5)
+player = Player(player_image, 100, 100, 50, 50, 3)
 game = True
+
+for i in range(10):
+    enemy = Enemy(zombie_images[0], 100, 100, 50, 50, 1.5)
+    enemy.spawn()
+    enemies.add(enemy)
 
 while True:
     for event in pygame.event.get():
@@ -9,6 +14,13 @@ while True:
 
     if game:
         win.blit(background_image, (0, 0))
+        for enemy in enemies:
+            dx = enemy.rect.centerx - player.rect.centerx
+            dy = player.rect.centery - enemy.rect.centery
+            ang = -math.atan2(dy, dx) - math.pi
+            enemy.update(ang)
+            enemy.draw()
+
         player.update()
         player.draw()
         bullets.update()
